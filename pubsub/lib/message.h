ae7a6c404f9842e67e6822eacf465fb4b482d631
Philippe PITTOLI <p.pittoli@unistra.fr>
2018-10-09 16:38:27 +0200

    message.[ch] from pubsub

---
+++ pubsub/lib/message.h
@@ -0,0 +1,21 @@
+#ifndef __PUBSUB_MSG_H__
+#define __PUBSUB_MSG_H__
+
+#define PUBSUB_MSG_TYPE_SUB         1
+#define PUBSUB_MSG_TYPE_UNSUB       2
+#define PUBSUB_MSG_TYPE_PUB         3
+
+struct pubsub_msg {
+    unsigned char type; // message type : alert, notification, …
+    char *chan;
+    size_t chanlen;
+    char *data;
+    size_t datalen;
+};
+
+void pubsub_message_serialize (const struct pubsub_msg *msg, char **data, size_t *len);
+void pubsub_message_unserialize (struct pubsub_msg *msg, const char *data, size_t len);
+void pubsub_message_free (struct pubsub_msg *msg);
+void pubsub_message_print (const struct pubsub_msg *msg);
+
+#endif
